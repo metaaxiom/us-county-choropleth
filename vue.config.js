2b049484c1ed58fa59e6f9a586f4721bec10914e
Julian <9219434+metaaxiom@users.noreply.github.com>
2022-10-26 01:11:26 -0400

    docker fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/app/population-choropleth/" : "/",
   configureWebpack: {
     devServer: {
-      disableHostCheck: true
-    }
-  }
-}
+      disableHostCheck: true,
+    },
+  },
+};
